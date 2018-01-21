29bfcc7dfba65cec19f40054ff81d4762734ceb4
Christian Mueller <smoki99@googlemail.com>
2018-01-21 18:30:20 +0100

    added simple script to push image

---
+++ alpine-core-builder/README.md
@@ -0,0 +1,9 @@
+# Alpine Core Builder
+
+Inspired by https://github.com/gliderlabs/docker-alpine/tree/master/builder
+
+This docker container is used to build the alpine-core container itself
+
+
+# Run in your docker
+
---
+++ alpine-core-builder/createCoreAlpine.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Create the base directory for the new docker core container
+rootfs="/var/tmp/docker-rootfs"
+mkdir $rootfs
+
+# Create the apk directory for target
+ mkdir $rootfs/etc/apk -p
+
+ echo $repositoryurl/main > $rootfs/etc/apk/repositories
+ echo $repositoryurl/community >> $rootfs/etc/apk/repositories
+
+# Install base packages into target
+apk --root "$rootfs" --update-cache --keys-dir /etc/apk/keys add --arch x86_64 --initdb ${packages[*]//,/ }
+
+# Cleanup Cache
+rm -f "$rootfs/var/cache/apk"/*
+
+# Security: Disable root password!
+sed -ie 's/^root::/root:!:/' "$rootfs/etc/shadow"
+
+tar -J -f rootfs.tar.xz --numeric-owner --exclude='dev/*' -C "$rootfs" -c .
+
+cat rootfs.tar.xz
