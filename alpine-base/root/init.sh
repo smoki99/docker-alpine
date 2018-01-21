296c263466ad67cfd82c05ee9bcc6398db5f0baa
Christian Mueller <smoki99@googlemail.com>
2018-01-21 21:36:18 +0100

    changed init script a little

--- alpine-base/root/init.sh
+++ alpine-base/root/init.sh
@@ -1,1 +1,2 @@
-  go-dnsmasq --default-resolver --ndots "1" --fwd-ndots "0" --hostsfile=/etc/hosts >> /var/log/dns.log 2>&1
+ #!/bin/sh
+ go-dnsmasq --default-resolver --ndots "1" --fwd-ndots "0" --hostsfile=/etc/hosts >> /var/log/dns.log 2>&1
